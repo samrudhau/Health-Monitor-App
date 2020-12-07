ed872fb609ab7db17e9a4dd4e21489b60d9207a2
Bao Nguyen <797nguyenbao@gmail.com>
2020-12-07 05:31:25 -0800

    Scatter plot now displays on same domain

--- vendor/line-chart.js
+++ vendor/line-chart.js
@@ -1,82 +1,41 @@
 
 $.getJSON('/data', function(user) {
-  let netCals = [];
   let dates = [];
-  let dates2 = [];
-
-  var objSentFromSrv = user.userData;
-  dates = objSentFromSrv.date;
-  dates2 = objSentFromSrv.date;
+  dates = user.userData.date;
  
   if (dates.length > 0)
   {
-    var i;
-  var j;
-  for (i=0; i < objSentFromSrv.date.length; i++){
-    j = objSentFromSrv.caloriesIn[i] - objSentFromSrv.caloriesOut[i];
-    netCals.push(j);
-    dates[i] = dates[i].slice(5, 10);
-  };
+    let combinedDatesAndCals = [];
+    combinedDatesAndCals.push(['dates', 'weight']);
 
-  
-  
-  var m; 
-  let combinedDatesAndCals = [];
-  combinedDatesAndCals.push(['dates', 'weight']);
-  for (m=0; m<dates.length; m++){
-    
-    combinedDatesAndCals.push([dates[m], netCals[m]]);
-
-  };
-  var g;
-  let objectArrayDates = [];
-  for(g=0; g<dates2.length; g++){
-    
-    objectArrayDates.push({date: dates2[g], netCals: netCals[g]});
-  };
-  objectArrayDates.sort(function(a, b) {
-    // convert date object into number to resolve issue in typescript
-    return  +new Date(a.date) - +new Date(b.date);
-  })
-
-  var ed;
-  let sortedDatesadnNetCals = [];
-  sortedDatesadnNetCals.push(['date', 'net calories']);
-  for(ed = 0; ed < dates2.length; ed++){
-    sortedDatesadnNetCals.push([objectArrayDates[ed].date, objectArrayDates[ed].netCals]);
-
-  };
-  
-
-  render(sortedDatesadnNetCals);
-
+    for (var i=0; i < dates.length; i++){
+      var net = user.userData.caloriesIn[i] - user.userData.caloriesOut[i];
+      var dateString = dates[i].split('-');
+      var date = new Date(dateString[0], dateString[1], dateString[2]);
+      combinedDatesAndCals.push([ date, net ]);
+    };
+    render(combinedDatesAndCals);
   }
   else{
     render( [ ['date', 'net calories'], ['No Data', 0] ] );
-
   };
-
-  
-  
 });
+
 function render(combinedDatesAndCals){
+  
   google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
 
+
       function drawChart() {
         var data = google.visualization.arrayToDataTable(combinedDatesAndCals);
-
         var options = {
           title: '',
-          hAxis: {title: 'Dates', minValue: 0, maxValue: 15},
-          vAxis: {title: 'Net Calories', minValue: 0, maxValue: 15},
+          hAxis: {title: 'Dates', format: 'd MMM'},
+          vAxis: {title: 'Net Calories'},
           legend: 'none'
         };
-
         var chart = new google.visualization.ScatterChart(document.getElementById('scatter-plot'));
-
         chart.draw(data, options);
       }
-
-
 };
