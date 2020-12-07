9e8c1c7ec52ed8b6d6d8cb73386d07c9b4bae37e
Bao Nguyen <797nguyenbao@gmail.com>
2020-12-07 02:41:51 -0800

    Reset Password page min 6 char type->password

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const User = require('../models/user')
 
 router.get('/', (req, res) => {
-    res.render('resetPassword', {passwordsNotMatching: req.flash('passwordsNotMatching') });
+    res.render('resetPassword', {
+        passwordsNotMatching: req.flash('passwordsNotMatching'),
+        minLength: req.flash('minLength')
+    });
 });
 
 //user presses Register button from login page, redirect to register page
@@ -12,6 +15,14 @@ router.post('/resetPassword', (req, res) => {
     {
         req.flash('passwordsNotMatching', 'Passwords do not match');
         console.log('passwords do not match');
+        req.session.save(function() {
+            res.redirect('/resetPassword');
+        });
+    }
+    else if (req.body.password.length < 6)
+    {
+        req.flash('minLength', 'Password must have 6 characters minimum');
+        console.log('must have 6 characters minimum');
         req.session.save(function() {
             res.redirect('/resetPassword');
         });
